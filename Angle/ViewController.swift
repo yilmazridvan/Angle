caf63570ea22f6df210f3ac4170ee5cb1a3d81e2
Rıdvan Yılmaz <ridvanyilmaz5098@gmail.com>
2022-10-20 15:14:47 +0300

    Initial Commit

---
+++ Angle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Angle
+//
+//  Created by Rıdvan Yılmaz on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
